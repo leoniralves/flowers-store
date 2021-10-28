8fe712f95ebb5e1090d02d008d2dfa266cb0477f
Gabriel Henrique <gabhenrique0@gmail.com>
2021-10-28 09:19:46 -0300

    :wrench: Chore: create first flowers list

---
+++ FlowersStore/FlowersListViewController.swift
@@ -0,0 +1,15 @@
+//
+//  FlowersListViewController.swift
+//  FlowersStore
+//
+//  Created by Gabriel Pereira on 28/10/21.
+//
+
+import UIKit
+
+final class FlowersListViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
