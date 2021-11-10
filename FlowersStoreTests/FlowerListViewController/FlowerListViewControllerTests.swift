46b3dc2d351a6ab14c05cd0b5bf8af69b7196a47
Mag <magnoaugustofr@gmail.com>
2021-11-10 09:24:27 -0300

    Create test for FlowersListViewController

---
+++ FlowersStoreTests/FlowerListViewController/FlowerListViewControllerTests.swift
@@ -0,0 +1,22 @@
+//
+//  FlowerListViewControllerTests.swift
+//  FlowersStoreTests
+//
+//  Created by Magno Ferreira on 10/11/21.
+//
+
+import Foundation
+import XCTest
+@testable import FlowersStore
+
+final class FlowerListViewControllerTests: XCTestCase {
+    private lazy var sut: FlowersListViewController = {
+        let sut: FlowersListViewController = .init()
+        sut.loadViewIfNeeded()
+        return sut
+    }()
+
+    func test_bla() {
+        
+    }
+}
