60228ef2cd28f011c9b18e96ef572b350a422e15
Leonir Deolindo <leonir.ad@gmail.com>
2022-04-22 18:14:00 -0300

    teste danger 18

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,11 +1,11 @@
 import Danger 
-import DangerSwiftCoverage
+// import DangerSwiftCoverage
 
 let danger = Danger()
 
 let editedFiles = danger.git.modifiedFiles + danger.git.createdFiles
 message("These files have changed: \(editedFiles.joined(separator: ", "))")
 
-Coverage.xcodeBuildCoverage(.derivedDataFolder("Build"), 
-                            minimumCoverage: 50, 
-                            excludedTargets: ["DangerSwiftCoverageTests.xctest"])
+// Coverage.xcodeBuildCoverage(.derivedDataFolder("Build"), 
+//                             minimumCoverage: 50, 
+//                             excludedTargets: ["DangerSwiftCoverageTests.xctest"])
