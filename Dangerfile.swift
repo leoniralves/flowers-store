3f293710f8b47316aa451f6d925c2912ebf9310f
Leonir Deolindo <leonir.ad@gmail.com>
2022-04-25 19:01:36 -0300

    pods, test e danger 1 - Danger Summary

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,10 +1,14 @@
 import Danger 
 import DangerSwiftCoverage
+import DangerXCodeSummary
 
 let danger = Danger()
 
 let editedFiles = danger.git.modifiedFiles + danger.git.createdFiles
 message("These files have changed: \(editedFiles.joined(separator: ", "))")
+
+let summary = XCodeSummary(filePath: "result.json")
+summary.report()
 
 Coverage.xcodeBuildCoverage(.derivedDataFolder("Build"), 
                             minimumCoverage: 90, 
