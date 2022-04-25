d13ce02a683b1fbe28c563c4449920328f6ab0f7
Leonir Deolindo <leonir.ad@gmail.com>
2022-04-25 12:19:09 -0300

    pods, test e danger 4 - sem coverage

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,11 +1,6 @@
 import Danger 
-// import DangerSwiftCoverage
 
 let danger = Danger()
 
 let editedFiles = danger.git.modifiedFiles + danger.git.createdFiles
 message("These files have changed: \(editedFiles.joined(separator: ", "))")
-
-// Coverage.xcodeBuildCoverage(.derivedDataFolder("Build"), 
-//                             minimumCoverage: 100, 
-//                             excludedTargets: ["DangerSwiftCoverageTests.xctest"])
