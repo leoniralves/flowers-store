d5c8d8683c3f2afa25df8b16542d51a782b8cb56
Leonir Deolindo <leonir.ad@gmail.com>
2022-04-26 18:47:41 -0300

    pods, test e danger 7 - Periphery

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -5,19 +5,17 @@ import DangerSwiftPeriphery
 
 let danger = Danger()
 
-// let editedFiles = danger.git.modifiedFiles + danger.git.createdFiles
-// message("These files have changed: \(editedFiles.joined(separator: ", "))")
+let editedFiles = danger.git.modifiedFiles + danger.git.createdFiles
+message("These files have changed: \(editedFiles.joined(separator: ", "))")
 
-// let summary = XCodeSummary(filePath: "result.json")
-// summary.report()
+let summary = XCodeSummary(filePath: "result.json")
+summary.report()
 
-// Coverage.xcodeBuildCoverage(.derivedDataFolder("Build"), 
-//                             minimumCoverage: 90, 
-//                             excludedTargets: ["FlowersStoreTests.xctest"])
+Coverage.xcodeBuildCoverage(.derivedDataFolder("Build"), 
+                            minimumCoverage: 90, 
+                            excludedTargets: ["FlowersStoreTests.xctest"])
 
-// DangerPeriphery.scan(arguments: [
-//     "--index-store-path Build/Index/DataStore",
-//     "--skip-build"
-// ])
-
-DangerPeriphery.scan()
+DangerPeriphery.scan(arguments: [
+    "--index-store-path Build/Index/DataStore",
+    "--skip-build"
+])
