87b243d2693b6a5ec7fd2a1fd080f5deb1af5ddb
Leonir Deolindo <leonir.ad@gmail.com>
2022-04-27 20:23:23 -0300

    pods, test e danger 31 - Periphery com SPM

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -5,6 +5,8 @@ import DangerSwiftPeriphery
 
 let danger = Danger()
 
+DangerPeriphery.scan()
+
 let editedFiles = danger.git.modifiedFiles + danger.git.createdFiles
 message("These files have changed: \(editedFiles.joined(separator: ", "))")
 
@@ -14,13 +16,3 @@ summary.report()
 Coverage.xcodeBuildCoverage(.derivedDataFolder("Build"), 
                             minimumCoverage: 90, 
                             excludedTargets: ["FlowersStoreTests.xctest"])
-
-// DangerPeriphery.scan(arguments: [
-//     "--workspace FlowersStore.xcworkspace",
-//     "--schemes FlowersStore",
-//     "--targets FlowersStore,FlowersStoreTests",
-//     "--index-store-path Build/Index/DataStore",
-//     "--skip-build"
-// ])
-
-DangerPeriphery.scan()
