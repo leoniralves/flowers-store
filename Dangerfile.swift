97eb69e462d2b9e905bc9789b63373976cc47d21
Leonir Deolindo <leonir.ad@gmail.com>
2022-05-02 11:55:40 -0300

    pods, test e danger 13 - Periphery OK

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,18 +1,3 @@
-import Danger 
-import DangerSwiftCoverage
-import DangerXCodeSummary
 import DangerSwiftPeriphery
 
-let danger = Danger()
-
 DangerPeriphery.scan()
-
-// let editedFiles = danger.git.modifiedFiles + danger.git.createdFiles
-// message("These files have changed: \(editedFiles.joined(separator: ", "))")
-
-// let summary = XCodeSummary(filePath: "result.json")
-// summary.report()
-
-// Coverage.xcodeBuildCoverage(.derivedDataFolder("Build"), 
-//                             minimumCoverage: 90, 
-//                             excludedTargets: ["FlowersStoreTests.xctest"])
